4fb9ce4ee94b15dfb8966ae111147730167c236b
Brian <58400498+nairb725@users.noreply.github.com>
2024-01-20 14:40:59 +0100

    grandpa is walking like chief

--- Assets/Scripts/GrandpaWalk.cs
+++ Assets/Scripts/GrandpaWalk.cs
@@ -8,16 +8,34 @@ public class GrandpaWalk : MonoBehaviour
     [SerializeField]
     private float SpeedCharacter;
 
+    [SerializeField]
+    private int LifePoint;
+
+    [SerializeField]
+    private bool _isGoingRight = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Invoke("goingRight", 0.33f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.right * SpeedCharacter * Time.smoothDeltaTime);
+        if(_isGoingRight == true)
+        {
+            transform.Translate(Vector3.right * SpeedCharacter * Time.smoothDeltaTime);
+            transform.Translate(Vector3.up * SpeedCharacter * Time.smoothDeltaTime);
 
+        }
+        {       
+            transform.Translate(Vector3.down * SpeedCharacter * Time.smoothDeltaTime);
+        }
+    }
+
+    void goingRight()
+    {
+        _isGoingRight = true;
     }
 }
