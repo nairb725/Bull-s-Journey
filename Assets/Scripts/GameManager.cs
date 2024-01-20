1674e816dd4b9e6d28e130bac4e4ff2e08aa48ba
Infernaton <lubin.pauly@gmail.com>
2024-01-20 14:39:16 +0100

    Add gamemanager + clikqble UI

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] GameObject m_UI;
+    [SerializeField] TrackingCamera m_Camera;
+    [SerializeField] PlayerController m_Player;
+
+    public void DisplayUI(bool display) => m_UI.SetActive(display);
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +20,25 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void Lauchgame()
+    {
+        DisplayUI(false);
+        Invoke(nameof(StartTracking), 2f);
+    }
+
+    public void Quit()
+    {
+        //Quit app
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+        Application.Quit();
+     }
+
+    private void StartTracking()
+    {
+        m_Camera.Track();
     }
 }
