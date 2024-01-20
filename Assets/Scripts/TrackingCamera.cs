99db345173ac7cddaa5cff5e798efd634a3c5336
Infernaton <lubin.pauly@gmail.com>
2024-01-20 13:49:35 +0100

    Grid in prefab + tracking camera script

---
+++ Assets/Scripts/TrackingCamera.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackingCamera : MonoBehaviour
+{
+    private bool _isTracking;
+    [SerializeField] GameObject m_TargetTracking;
+
+    public void Track() => _isTracking = true;
+
+    public void Untrack() => _isTracking = false;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (_isTracking)
+            transform.position = new Vector3(m_TargetTracking.transform.position.x, m_TargetTracking.transform.position.y, transform.position.z);
+        
+    }
+}
